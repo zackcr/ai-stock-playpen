35bd43dbb41de006fd9134afc988213b3d903233
zackcr <zackcr@gmail.com>
2026-07-11 17:53:12 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "playpen-v6";
+const CACHE = "playpen-v8";
 const SHELL = ["./", "./index.html", "./app.js", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
